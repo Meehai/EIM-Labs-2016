b4334d78a7a28135dacb78124bdc9bd9454e29d8
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2016-03-15 17:30:54 +0200

    Finished lab 3. Added landscape and portrait layouts. Added callbacks to buttons and set permission to call the number from edittext

---
+++ Laborator03/PhoneDialer/app/src/main/java/lab03/eim/systems/cs/pub/ro/phonedialer/Constants.java
@@ -0,0 +1,21 @@
+package lab03.eim.systems.cs.pub.ro.phonedialer;
+
+/**
+ * Created by mihai on 15/03/16.
+ */
+public class Constants {
+    public static final int[] buttonIds = {
+            R.id.number_1_button,
+            R.id.number_2_button,
+            R.id.number_3_button,
+            R.id.number_4_button,
+            R.id.number_5_button,
+            R.id.number_6_button,
+            R.id.number_7_button,
+            R.id.number_8_button,
+            R.id.number_9_button,
+            R.id.number_0_button,
+            R.id.star_button,
+            R.id.pound_button
+    };
+}
--- Laborator03/PhoneDialer/app/src/main/java/lab03/eim/systems/cs/pub/ro/phonedialer/PhoneDialerActivity.java
+++ Laborator03/PhoneDialer/app/src/main/java/lab03/eim/systems/cs/pub/ro/phonedialer/PhoneDialerActivity.java
@@ -1,13 +1,23 @@
 package lab03.eim.systems.cs.pub.ro.phonedialer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PhoneDialerActivity extends AppCompatActivity {
 
@@ -17,6 +27,8 @@ public class PhoneDialerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phone_dialer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        this.setHandlers();
     }
 
     @Override
@@ -39,5 +51,61 @@ public class PhoneDialerActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setHandlers() {
+
+        final EditText editText = (EditText)findViewById(R.id.phone_number_edit_text);
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                editText.setText(editText.getText().toString() + button.getText().toString());
+            }
+        };
+
+        View.OnClickListener backspaceButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextString = editText.getText().toString();
+
+                if(editTextString.length() == 0)
+                    return;
+
+                editText.setText( editTextString.substring(0, editTextString.length()-1) );
+            }
+        };
+
+        View.OnClickListener hangupButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+                finish();
+            }
+        };
+
+        View.OnClickListener callButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(intent);
+                } else {
+                    // Iau eroare aici la castarea getApplicationContext la Activity (??)
+                    //ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.CALL_PHONE}, 1);
+                    Log.d("[PhoneDialer]", "Application does not have calling permissions.");
+                }
+            }
+        };
+
+        for (int i=0; i<Constants.buttonIds.length; i++)
+            findViewById(Constants.buttonIds[i]).setOnClickListener(numberButtonClickListener);
+
+        findViewById(R.id.backspace_image_button).setOnClickListener(backspaceButtonClickListener);
+        findViewById(R.id.hangup_image_button).setOnClickListener(hangupButtonClickListener);
+        findViewById(R.id.call_image_button).setOnClickListener(callButtonClickListener);
     }
 }
